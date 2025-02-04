0d82f9e6939c732db59ed6f88a1ee9fcb507a87a
Abdur Rahim <abdurrahim300431@gmail.com>
2025-02-04 12:52:16 +0600

    Agency site setup & upload

---
+++ wp-content/themes/mediplus/index.php
@@ -0,0 +1,62 @@
+<?php
+get_header();
+?>
+		
+		<!-- Slider Area -->
+		<?php get_template_part('template-parts/homepage/slider'); ?>
+		<!--/ End Slider Area -->
+		
+		<!-- Start Schedule Area -->
+		<?php get_template_part('template-parts/homepage/schedule'); ?>
+		<!--/End Start schedule Area -->
+
+		<!-- Start Feautes -->
+		<?php get_template_part('template-parts/homepage/feautes'); ?>
+		<!--/ End Feautes -->
+		
+		<!-- Start Fun-facts -->
+		<?php get_template_part('template-parts/homepage/funfacts')?>
+		<!--/ End Fun-facts -->
+		
+		<!-- Start Why choose -->
+		<?php get_template_part('template-parts/homepage/whychoose')?>
+		<!--/ End Why choose -->
+		
+		<!-- Start Call to action -->
+		<?php get_template_part('template-parts/homepage/calltoaction')?>
+		<!--/ End Call to action -->
+		
+		<!-- Start portfolio -->
+		<?php get_template_part('template-parts/homepage/portfolio')?>
+		<!--/ End portfolio -->
+		
+		<!-- Start service -->
+		<?php get_template_part('template-parts/homepage/services')?>
+		<!--/ End service -->
+		
+		<!-- Pricing Table -->
+		<?php get_template_part('template-parts/homepage/pricingTable')?>
+		<!--/ End Pricing Table -->
+		
+		<!-- Start Blog Area -->
+		<?php get_template_part('template-parts/homepage/blog')?>
+		<!-- End Blog Area -->
+		
+		<!-- Start clients -->
+		<?php get_template_part('template-parts/homepage/client')?>
+		<!--/Ens clients -->
+		
+		<!-- Start Appointment -->
+		<?php get_template_part('template-parts/homepage/appointment')?>
+		<!-- End Appointment -->
+		
+		<!-- Start Newsletter Area -->
+		<?php get_template_part('template-parts/homepage/newsletter')?>
+		<!-- /End Newsletter Area -->
+		
+		
+
+
+<?php
+get_footer();
+?>
